98e36d33ad4af8c80596143fcbc131e5bc12fb98
mga102 <kkww94@gmail.com>
2024-06-18 20:53:34 +0900

    plugins 폴더 추가

---
+++ plugins/shell/selected_fruit.sh
@@ -0,0 +1,11 @@
+
+if [$1 == APPLE]; then 
+    echo "You selected Apple!"
+elif [ $1 == ORANGE ]; then
+    echo "You selected ORANGE!"
+elif [ $1 == GRAPE ]; then
+    echo "You selected GRAPE!"
+else 
+    echo "You selected other Fruit!"
+fi
+:
