1829efe986cbd426bc542017cf24c04b6f68ca63
mga102 <kkww94@gmail.com>
2024-06-25 20:15:22 +0900

    python with macro 업로드

---
+++ dags/dags_python_with_macro.py
@@ -0,0 +1,28 @@
+from airflow.models.dag import DAG
+import pendulum
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="dags_python_with_macro",
+    schedule= "10 0 * * *",
+    start_date=pendulum.datetime(2024, 4, 4, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id = 'task_using_macros',
+      templates_dict = {'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1,days =1))| ds }}',
+                        'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day =1) +macros.dateutil.relativedelta.relativedelta(days =1)) |ds}}'
+        }
+    )
+
+    def get_datetime_macro(**kwargs):
+        templates_dict = kwargs.get('templates_dict') or {}
+        if templates_dict:
+            start_date = templates_dict.get('start_date') or 'start_date 없음'
+            end_date = templates_dict.get('end_date') or 'end_date 없음'
+            print(start_date)
+            print(end_date)
+
+
+get_datetime_macro()
